b81b2e92ac68c4f86e90805e9d30caa9a19b6ed6
uzmahamid01 <uzma_hamid@tamu.edu>
2024-02-03 11:00:41 -0600

    R done

--- recursion/reverseWord.py
+++ recursion/reverseWord.py
@@ -13,3 +13,13 @@ def reverseWord(str):
 
 
 print(reverseWord("yoyo master"))
+
+'''If the input string is not empty, the function recursively calls itself with the substring starting from the second 
+character (str.substr(1)), and then concatenates the first character (str.charAt(0)) at the end. '''
+
+def reverse(str):
+    if str == "":
+        return ""
+    return reverse(str[1:]) + str[0]
+
+print(reverse("yoyo master"))
