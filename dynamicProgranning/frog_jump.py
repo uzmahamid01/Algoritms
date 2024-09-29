7134e22d89b52c503e2992222e0058ac1148fbd4
uzmahamid01 <uzma_hamid@tamu.edu>
2024-09-29 14:35:48 -0500

    dp frog jump

--- dynamicProgranning/frog_jump.py
+++ dynamicProgranning/frog_jump.py
@@ -1,25 +1,51 @@
 class Solution:
-    def f(self, index, heights, dp):
-        if index == 0:
-            return 0
-        if dp[index] != -1:
-            return dp[index]
+    # def f(self, index, heights, dp):
+    #     if index == 0:
+    #         return 0
+    #     if dp[index] != -1:
+    #         return dp[index]
      
-        left = self.f(index - 1, heights, dp) + abs(heights[index] - heights[index-1])
+    #     left = self.f(index - 1, heights, dp) + abs(heights[index] - heights[index-1])
         
-        if index >= 2:
-            right = self.f(index - 2, heights, dp) + abs(heights[index] - heights[index-2])
-            dp[index] = min(left, right)
-        else:
-            dp[index] = left
+    #     if index >= 2:
+    #         right = self.f(index - 2, heights, dp) + abs(heights[index] - heights[index-2])
+    #         dp[index] = min(left, right)
+    #     else:
+    #         dp[index] = left
         
-        return dp[index]
+    #     return dp[index]
     
+    #tabulation form
+    # def frog_jump(self, n, heights):
+    #memoization(recurssion form)
+    #     # dp = [-1] * (n + 1)
+    #     # return self.f(n-1, heights, dp)
 
-    def frog_jump(self, n, heights):
-        dp = [-1] * (n + 1)
-        return self.f(n-1, heights, dp)
+    #     dp = [0] * (n)
+    #     dp[0] = 0
+    #     for i in range(1, n):
+    #         fs = dp[i-1] + abs(heights[i] - heights[i-1])
+    #         ss = float('inf')
+    #         if i > 1:
+    #             ss = dp[i-2] + abs(heights[i] - heights[i-2])
+    #         dp[i] = min(fs, ss)
+
+    #     return dp[n-1]
     
+    #space optimize
+    def frog_jump2(self, n, heights):
+        prev, prev2 = 0, 0
+
+        for i in range(1, n):
+            fs = prev + abs(heights[i] - heights[i-1])
+            ss = float('inf')
+            if i > 1:
+                ss = prev2 + abs(heights[i] - heights[i-2])
+            curr = min(fs, ss)
+            prev2 = prev
+            prev = curr
+
+        return prev
 
 def main():
     # Input parameters
@@ -30,7 +56,7 @@ def main():
     solution = Solution()
 
     # Call the frog_jump function
-    result = solution.frog_jump(n, heights)
+    result = solution.frog_jump2(n, heights)
 
     # Print the result
     print(f"Minimum energy needed: {result}")
